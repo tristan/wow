c4b6a705968f18dbce3675ad735c1c67c772e787
Tristan King <tristan.king@gmail.com>
2010-11-30 18:24:34 +0100

    first commit

---
+++ gearer/project.clj
@@ -0,0 +1,8 @@
+(defproject gearer "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+		 [org.clojure/clojure-contrib "1.2.0"]
+;		 [http.async.client "0.2.1"]
+		 [clj-http "0.1.1"]
+		 ]
+  :dev-dependencies [[swank-clojure "1.2.0"]])
---
+++ gearer/test/gearer/test/core.clj
@@ -0,0 +1,6 @@
+(ns gearer.test.core
+  (:use [gearer.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
